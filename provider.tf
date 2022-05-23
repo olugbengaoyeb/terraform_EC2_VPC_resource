14d9d5a157200c9b3c73425b2dd66b9128f400a6
olugbengaoyeb <98466439+olugbengaoyeb@users.noreply.github.com>
2022-05-23 17:22:52 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "ca-central-1"
-  access_key = "AKIA6LAQVREDPWKWLHEM"
-  secret_key = "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"
+  access_key = "A*********************J"
+  secret_key = "M****************************Uyuuu****"
   alias      = "central"
 }
